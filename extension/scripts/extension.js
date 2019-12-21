80a7cbe0b03dc26bd28a2a4b762c801185e7cfd5
cardona150 <575891+cardona150@users.noreply.github.com>
2019-12-21 01:15:50 -0800

    V2

--- extension/scripts/extension.js
+++ extension/scripts/extension.js
@@ -1,8 +1,15 @@
-// #vehicleContent loads after document_idle
+// This is a waste of space
+$("#banner-GTAV").remove()
 
-var observer = new MutationObserver(MutationsCallback)
-observer.observe(document.getElementById("sectionSinglePlayer"), {childList:true, subtree:true})
-function MutationsCallback(mutations, observer)
+// This really isn't necessary
+$("#getApp").remove()
+
+// Personally, I don't like this
+$("#summaryStats").remove()
+
+var sectionSinglePlayerObserver = new MutationObserver(SectionSinglePlayerObserverMutationsCallback)
+sectionSinglePlayerObserver.observe(document.getElementById("sectionSinglePlayer"), {childList:true, subtree:true})
+function SectionSinglePlayerObserverMutationsCallback(mutations, observer)
 {
   for(const mutation of mutations)
   {
@@ -10,8 +17,12 @@ function MutationsCallback(mutations, observer)
     {
       if(addedNode.id=="vehicleTypes")
       {
-        // BINGO
+        // TODO: This is messing up the "GARAGE" tab
         vehicleTypes()
+      }
+      if(addedNode.id=="gunTypes")
+      {
+        gunTypes()
       }
     }
   }
@@ -22,15 +33,14 @@ function vehicleTypes() {
   <table id="gtavie">
     <thead>
       <tr>
-        <th data-sort="string">Name</th>
-        <th data-sort="float">Speed</th>
-        <th data-sort="float">Acceleration</th>
-        <th data-sort="float">Braking</th>
-        <th data-sort="float">Handling</th>
+        <th data-sort="string" data-sort-default="desc">Name</th>
+        <th data-sort="float" data-sort-default="desc">Speed</th>
+        <th data-sort="float" data-sort-default="desc">Acceleration</th>
+        <th data-sort="float" data-sort-default="desc">Braking</th>
+        <th data-sort="float" data-sort-default="desc">Handling</th>
         <th data-sort="float" data-sort-default="desc" data-sort-onload=yes>S+A</th>
-        <th data-sort="float">S+A+B</th>
-        <th data-sort="float">S+A+H</th>
-        <th data-sort="float">S+A+B+H</th>
+        <th data-sort="float" data-sort-default="desc">S+A+H</th>
+        <th data-sort="float" data-sort-default="desc">S+A+B+H</th>
       </tr>
     </thead>
   </table>`)
@@ -38,7 +48,7 @@ function vehicleTypes() {
   body = $("<tbody></tbody>")
   table.append(body)
 
-  $(".vehicle").each(function(index, value) {
+  $("#vehicleTypes .vehicle").each(function(index, value) {
     row = $("<tr></tr>")
     body.append(row)
 
@@ -53,7 +63,6 @@ function vehicleTypes() {
     row.append(`<td>${fixed(handling)}</td>`)
     
     row.append(`<td>${fixed(speed+acceleration)}</td>`)
-    row.append(`<td>${fixed(speed+acceleration+braking)}</td>`)
     row.append(`<td>${fixed(speed+acceleration+handling)}</td>`)
     row.append(`<td>${fixed(speed+acceleration+braking+handling)}</td>`)
 
@@ -61,7 +70,86 @@ function vehicleTypes() {
   })
 
   table.stupidtable()
-  $(".gridRow").replaceWith(table)
+  //$(".gridRow").replaceWith(table)
+  $("#vehicleTypes").replaceWith(table)
+}
+
+function gunTypes() {
+  table = $(`
+  <table id="gtavie">
+    <thead>
+      <tr>
+        <th data-sort="string" data-sort-default="desc">Name</th>
+        <th data-sort="float" data-sort-default="desc">Damage</th>
+        <th data-sort="float" data-sort-default="desc">Fire Rate</th>
+        <th data-sort="float" data-sort-default="desc">Accuracy</th>
+        <th data-sort="float" data-sort-default="desc">Range</th>
+        <th data-sort="float" data-sort-default="desc">Clip Size</th>
+        <th data-sort="float" data-sort-default="desc" data-sort-onload=yes>D+F</th>
+        <th data-sort="float" data-sort-default="desc">D+F+A</th>
+        <th data-sort="float" data-sort-default="desc">D+F+C</th>
+        <th data-sort="float" data-sort-default="desc">D+F+A+R+C</th>
+      </tr>
+    </thead>
+  </table>`)
+  
+  body = $("<tbody></tbody>")
+  table.append(body)
+
+  $("#gunTypes .weapon").each(function(index, value) {
+    row = $("<tr></tr>")
+    body.append(row)
+
+    row.append(`<td>${$(value).attr("data-name")}</td>`)
+
+    progressBars = $(".progress-bar", this)
+
+    damage = Number($(".under", this)[0].innerText.slice(0, -1))
+    row.append(`<td>${damage}</td>`)
+
+    fireRate = Number($(".under", this)[1].innerText.slice(0, -1))
+    row.append(`<td>${fireRate}</td>`)
+
+    if(progressBars.length == 3)
+    {
+      accuracy = 0
+    }
+    else
+    {
+      accuracy = Number($(".under", this)[2].innerText.slice(0, -1))
+    }
+    row.append(`<td>${accuracy}</td>`)
+
+    if(progressBars.length == 3)
+    {
+      range = Number($(".under", this)[2].innerText.slice(0, -1))
+    }
+    else
+    {
+      range = Number($(".under", this)[3].innerText.slice(0, -1))
+    }
+    row.append(`<td>${range}</td>`)
+
+    if(progressBars.length != 5)
+    {
+      clipSize = 0
+    }
+    else
+    {
+      clipSize = Number($(".under", this)[4].innerText.slice(0, -1))
+    }
+    row.append(`<td>${clipSize}</td>`)
+
+    row.append(`<td>${damage+fireRate}</td>`)
+    row.append(`<td>${damage+fireRate+accuracy}</td>`)
+    row.append(`<td>${damage+fireRate+clipSize}</td>`)
+    row.append(`<td>${damage+fireRate+accuracy+range+clipSize}</td>`)
+
+    body.append(row)
+  })
+
+  table.stupidtable()
+  $("#gunTypes").replaceWith(table)
 }
 
 function fixed(x)
